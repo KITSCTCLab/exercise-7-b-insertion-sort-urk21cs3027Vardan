d2520973fb4c899c7c05dd61734376353b1db110
urk21cs3027Vardan <109951899+urk21cs3027Vardan@users.noreply.github.com>
2022-09-12 16:28:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,4 +19,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(insertion(data))
+
